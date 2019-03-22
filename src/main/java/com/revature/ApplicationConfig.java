e12920ff4dbb147c8033bb6f9fd6434c6596fbb9
Jose Rivera <joseuxs102@gmail.com>
2019-03-21 20:56:04 -0400

    Created User Controller. Controller has login and register methods ready to be hooked up to services

---
+++ src/main/java/com/revature/ApplicationConfig.java
@@ -0,0 +1,42 @@
+package com.revature;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * ApplicationConfig to setup the controller without
+ * any XML configuration. This will register a dispatch servlet
+ * in order to communicate with the front end.
+ * 
+ * @author Jose Rivera
+ *
+ */
+@EnableWebMvc
+@ComponentScan
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer, WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext container = new AnnotationConfigWebApplicationContext();
+		container.register(ApplicationConfig.class);
+
+		servletContext.addListener(new ContextLoaderListener(container));
+
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet",
+				new DispatcherServlet(container));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+	}
+
+}
---
+++ src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.revature.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.User;
+
+/**
+ * This controller will help with communicating with any requests that pertains
+ * to a user. /login will attempt to login a user. /register will attempt to
+ * register a user
+ * 
+ * @Endpoint /user
+ * 
+ * @author Jose Rivera
+ *
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	/**
+	 * Method to accept a POST request to login a user. Will accept a string array
+	 * that contains the user's credentials being the username and password.
+	 * 
+	 * @return The new user that was logged in, or null if the user was unable to be
+	 *         logged in.
+	 */
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User loginUser(@RequestBody String[] credentials) {
+
+		return null;
+	}
+
+	/**
+	 * Method to accept a POST request to register a user. Will accept a string
+	 * array that contains the user information to create a user and add them to the
+	 * database.
+	 * 
+	 * @return The new user that was added, or null if the user was unable to be
+	 *         added to the database.
+	 */
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public User registerUser(@RequestBody String[] registrationInfo) {
+
+		return null;
+	}
+}
