641355315299b2daf7f5fe6ee76ca9ca68e16e26
Jose Rivera <joseuxs102@gmail.com>
2019-03-23 20:46:29 -0400

    Added get by credentials to service layer. /auth endpoint in controller now calls the correct method. Errors with get by credentials and get by id

--- src/main/java/com/revature/repos/UserRepo.java
+++ src/main/java/com/revature/repos/UserRepo.java
@@ -7,7 +7,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-
 import com.revature.models.User;
 
 @Repository
@@ -19,46 +18,49 @@ public class UserRepo {
 	public UserRepo(SessionFactory factory) {
 		this.factory = factory;
 	}
-	
-	public List<User> getAll(){
+
+	public List<User> getAll() {
 		Session s = factory.getCurrentSession();
 		return s.createQuery("from Users", User.class).getResultList();
-		
+
 	}
-	
-	
+
 	public User getByCredentials(String username, String password) {
 		Session s = factory.getCurrentSession();
 		User u = s.get(User.class, username);
-		if(!(password == u.getPassword())) return null;
-		return u; 
-		
+		if (!(password == u.getPassword()))
+			return null;
+		return u;
+
 	}
-	
+
 	public User getById(int id) {
 		Session s = factory.getCurrentSession();
 		return s.get(User.class, id);
 	}
-	
+
 	public User add(User newUser) {
 		Session s = factory.getCurrentSession();
 		s.save(newUser);
 		return newUser;
 	}
-	
+
 	public User update(User updatedUser) {
 		Session s = factory.getCurrentSession();
-		User user = s.get(User.class,  updatedUser.getUser_id());
-		if(user == null) return null;
-		else user = updatedUser;
-		return user; 
+		User user = s.get(User.class, updatedUser.getUser_id());
+		if (user == null)
+			return null;
+		else
+			user = updatedUser;
+		return user;
 	}
-	
+
 	public boolean delete(int id) {
 		Session s = factory.getCurrentSession();
 		User user = s.get(User.class, id);
-		if(user == null) return false;
+		if (user == null)
+			return false;
 		s.delete(user);
-		return true; 
+		return true;
 	}
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -14,46 +14,52 @@ import com.revature.util.AesEncryptUtil;
 
 @Service
 public class UserService {
-	
+
 	private UserRepo userRepo;
-	
+
 	@Autowired
 	public UserService(UserRepo UserRepo) {
 		userRepo = UserRepo;
 	}
-	
-	@Transactional(readOnly=true, isolation=Isolation.SERIALIZABLE)
+
+	@Transactional(readOnly = true, isolation = Isolation.SERIALIZABLE)
 	public List<User> getAll() {
-		List<User> users= userRepo.getAll();
-		for(User u : users) {
+		List<User> users = userRepo.getAll();
+		for (User u : users) {
 			u.setPassword(AesEncryptUtil.decrypt(u.getPassword()));
 		}
 		return users;
 	}
-	
-	@Transactional(readOnly=true, isolation=Isolation.READ_COMMITTED)
+
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
 	public User getById(int id) {
 		User user = userRepo.getById(id);
 		user.setPassword(AesEncryptUtil.decrypt(user.getPassword()));
 		return user;
 	}
-	
-	@Transactional(isolation=Isolation.READ_COMMITTED, propagation=Propagation.REQUIRED)
+
+	@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
+	public User getByCredentials(String username, String password) {
+		User user = userRepo.getByCredentials(username, password);
+		user.setPassword(AesEncryptUtil.decrypt(user.getPassword()));
+		return user;
+	}
+
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 	public User add(User newUser) {
 		newUser.setPassword(AesEncryptUtil.encrypt(newUser.getPassword()));
 		return userRepo.add(newUser);
 	}
-	
-	@Transactional(propagation=Propagation.REQUIRES_NEW)
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public User update(User updatedUser) {
 		updatedUser.setPassword(AesEncryptUtil.encrypt(updatedUser.getPassword()));
 		return userRepo.update(updatedUser);
 	}
-	
+
 	@Transactional // default isolation = READ_UNCOMMITTED; default propagation = REQUIRED
 	public boolean delete(int id) {
 		return userRepo.delete(id);
 	}
-
 
 }
